11b44febd0a16bad6ace8d21c273ed83ed1622b2
gopaldhakal <gopaldhakal2000@gmail.com>
2024-06-29 14:53:17 +0545

    Initial commit of BBMS project

---
+++ index.php
@@ -0,0 +1,129 @@
+<?php
+error_reporting(0);
+include('includes/config.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>BloodBank & Donor Management System</title>
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="css/modern-business.css" rel="stylesheet">
+    <style>
+    .navbar-toggler {
+        z-index: 1;
+    }
+    
+    @media (max-width: 576px) {
+        nav > .container {
+            width: 100%;
+        }
+    }
+    .carousel-item.active,
+    .carousel-item-next,
+    .carousel-item-prev {
+        display: block;
+    }
+    </style>
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+<?php include('includes/header.php');?>
+<?php include('includes/slider.php');?>
+   
+    <!-- Page Content -->
+    <div class="container">
+
+        <h1 class="my-4">Welcome to RAKTADAN</h1>
+
+        <!-- Marketing Icons Section -->
+        <div class="row">
+            <div class="col-lg-4 mb-4">
+                <div class="card">
+                    <h4 class="card-header">The need for blood</h4>
+                   
+                        <p class="card-text" style="padding-left:2%">It is essential for surgeries, cancer treatment, chronic illnesses, and traumatic injuries. Whether a patient receives whole blood, red cells, platelets or plasma, this lifesaving care starts with one person making a generous donation. </p>
+                </div>
+            </div>
+            <div class="col-lg-4 mb-4">
+                <div class="card">
+                    <h4 class="card-header">Blood Tips</h4>
+                   
+                        <p class="card-text" style="padding-left:2%">Drink lots of water. Eat a salty snack.wear loose, comfortable clothing. Avoid heavy lifting and vigorous exercise. </p>
+                </div>
+            </div>
+            <div class="col-lg-4 mb-4">
+                <div class="card">
+                    <h4 class="card-header">Who you could Help</h4>
+                   
+                        <p class="card-text" style="padding-left:2%">Blood donors help patients of all ages: accident and burn victims, heart surgery and organ transplant patients, and those battling cancer.</p>
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Portfolio Section -->
+        
+        <!-- /.row -->
+
+        <!-- Features Section -->
+        <div class="row">
+            <div class="col-lg-6">
+                <h2>BLOOD GROUPS</h2>
+          <p>  blood group of any human being will mainly fall in any one of the following groups.</p>
+                <ul>
+                
+                
+<li>A positive or A negative</li>
+<li>B positive or B negative</li>
+<li>O positive or O negative</li>
+<li>AB positive or AB negative.</li>
+                </ul>
+                <p>A healthy diet helps ensure a successful blood donation, and also makes you feel better! Check out the following recommended foods to eat prior to your donation.</p>
+            </div>
+            <div class="col-lg-6">
+                <img class="img-fluid rounded" src="images/blood-donor (1).jpg" alt="">
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Call to Action Section -->
+        <div class="row mb-4">
+            <div class="col-md-8">
+            <h4>UNIVERSAL DONORS AND RECIPIENTS</h4>
+                <p>
+The most common blood type is O, followed by type A.
+
+Type O individuals are often called "universal donors" since their blood can be transfused into persons with any blood type. Those with type AB blood are called "universal recipients" because they can receive blood of any type.</p>
+            </div>
+            <div class="col-md-4">
+                <a class="btn btn-lg btn-secondary btn-block" href="become-donar.php">Become a Donar</a>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+  <?php include('includes/footer.php');?>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/tether/tether.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
